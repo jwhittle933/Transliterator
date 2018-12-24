7cff90ba78378fbdb3728f84e8ae83fa70708d76
Jonathan Whittle <jonathan.m.whittle@gmail.com>
2018-12-24 12:45:09 -0500

    remove /public, update home_handler

--- handler/home_handler.go
+++ handler/home_handler.go
@@ -8,8 +8,6 @@ import (
 
 //HomeHandler path controller
 func HomeHandler(c echo.Context) error {
-	// Please note the the second parameter "index.html" is the template name and should
-	// be equal to the value stated in the {{ define }} statement in "view/home.html"
 	return c.Render(http.StatusOK, "index.html", map[string]interface{}{
 		"name": "HOME",
 	})
