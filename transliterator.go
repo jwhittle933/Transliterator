791abc8e734998ec0f0e6569a8c03442809bfba6
Jonathan Whittle <jonathan.m.whittle@gmail.com>
2018-12-24 20:33:50 -0500

    langUp and langDown functions

--- transliterator.go
+++ transliterator.go
@@ -1,28 +1,13 @@
 package main
 
 import (
-	"io"
-	"text/template"
 	"transliterator/handler"
 
 	"github.com/labstack/echo"
 )
 
-// Template struct
-type Template struct {
-	templates *template.Template
-}
-
-// Render interface
-func (t *Template) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
-	return t.templates.ExecuteTemplate(w, name, data)
-}
-
 func main() {
 	e := echo.New()
-	e.Renderer = &Template{
-		templates: template.Must(template.ParseGlob("views/*.html")),
-	}
 	//e.File registers new route with static file to serve
 	e.File("/", "views/index.html")
 	e.File("/about", "views/about.html")
